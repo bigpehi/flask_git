1765545e2e5d6251b340804c1d0a1cfeaa9eee64
pehi <2281181505@qq.com>
2018-10-01 21:35:51 +0800

    10.1 21:35

--- forms.py
+++ forms.py
@@ -1,61 +1,8 @@
-# from flask import Flask,render_template,request
-# # 导入wtf扩展的表单类
-# from flask_wtf import FlaskForm
-# # 导入自定义表单需要的字段
-# from wtforms import SubmitField,StringField,PasswordField
-# # 导入wtf扩展提供的表单验证器
-# from wtforms.validators import DataRequired,EqualTo
-# # 导入配置好的相关数据库信息
-# from flask_sqlalchemy import SQLAlchemy
-
-# # 启动
-# app = Flask(__name__)
-# # 设置秘钥
-# app.config['SECRET_KEY'] = 'secret'
-
-
-# # 定义表单类
-# class Form_test(FlaskForm):
-#     username = StringField('用户名：',validators=[DataRequired()])
-#     password = PasswordField('密码：',validators=[DataRequired()])
-#     input = SubmitField('提交')
-
-# # 配置数据库并定义数据库表模型类
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost:3306/flask1'
-# app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
-# db = SQLAlchemy(app)
-
-
-# @app.route('/',methods=['GET','POST'])
-# def login():
-#     if request.method == "GET":
-#         # 显示登录页面
-#         form = Form_test()# 测试语句
-#         print("now is get")
-#         return render_template('login.html',form=form)
-#     if request.method == "POST":
-#         db.create_all()
-#         print("now is post")# 测试语句
-#         #从返回的request的值获取表单信息
-#         username = request.form.get('username')
-#         password = request.form.get('password')
-        
-#         admin = User(username,password)
-#         db.session.add(admin)
-#         db.session.commit()
-#         return "成功添加一个信息"
-
-
-
-
-# if __name__ == "__main__":
-#     app.run()
-
 from flask import Flask,render_template,request
 # 导入wtf扩展的表单类
 from flask_wtf import FlaskForm
 # 导入自定义表单需要的字段
-from wtforms import SubmitField,StringField,PasswordField
+from wtforms import SubmitField,StringField,PasswordField,RadioField
 # 导入wtf扩展提供的表单验证器
 from wtforms.validators import DataRequired,EqualTo
 # 导入配置好的相关数据库信息
@@ -65,51 +12,78 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 # 设置秘钥
 app.config['SECRET_KEY'] = 'secret'
-
+# 配置数据库并定义数据库表模型类
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost:3306/courses'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
+db = SQLAlchemy(app)
 
 # 定义表单类
 class Form_test(FlaskForm):
     username = StringField('用户名：',validators=[DataRequired()])
     password = PasswordField('密码：',validators=[DataRequired()])
+    radioButton = RadioField('method', choices=[(1,'学生'),(2,'教师')],default=1)
+
     # input = SubmitField('提交')
 
-# 配置数据库并定义数据库表模型类
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost:3306/flask1'
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
-db = SQLAlchemy(app)
-# 定义的类必须要继承db对象中的model类
+
+# 创建用户类 存储用户信息 用于登录
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(80),unique=False)
     password = db.Column(db.String(120),unique=False)
+    radio = db.Column(db.String(20),unique=False)
 
-    def __init__(self,username,password):
+    def __init__(self,username,password,radio):
         self.username = username
         self.password = password
+        self.radio  = radio
 
-@app.route('/index')
-def hello():
-    return 'hello'
+class Student(db.Model):
+    __tablename__='student'
+    number=db.Column(db.String(9),primary_key=True)
+    name=db.Column(db.String(20),unique=False)
+    sex=db.Column(db.String(20),unique=False)
+    age=db.Column(db.Integer,unique=False)
+
+class Course(db.Model):
+    __tablename__='course'
+    mingcheng=db.Column(db.String(20),primary_key=True)
+    teacher=db.Column(db.String(20),unique=True)
+    textbook=db.Column(db.String(20),unique=True)
+
+class Score(db.Model):
+    __tablename__='score'
+    id=db.Column(db.String(9),db.ForeignKey('student.number'),primary_key=True)
+    coursename=db.Column(db.String(20),db.ForeignKey('course.mingcheng'),primary_key=True)
+    score=db.Column(db.Integer)
+    # gzstudent=db.relationship('SStudent',backref='score')
+
 @app.route('/',methods=['GET','POST'])
 def login():
+    #实例化表单类
+    form = Form_test()
     if request.method == "GET":
-        # 显示登录页面
-        form = Form_test()
+
         # 测试语句
         print("now is get")
+        # 显示登录页面
         return render_template('login.html',form=form)
     if request.method == "POST":
-        print("now is post")# 测试语句
-        db.create_all()
+        print("now is post")# 测试语句)
         #从返回的request的值获取表单信息
-        username = request.form.get('username')
-        password = request.form.get('password')
-        
-        admin = User(username,password)
-        db.session.add(admin)
-        db.session.commit()
-        return "成功添加一个信息"
+        username = form.username.data
+        password = form.password.data
+        radio = "学生"
+        if form.radioButton.data == 2:
+            radio = "教师"
+        #验证该用户是否在数据库中
+        if User.query.filter_by(username=username).first():
+            return "登陆成功\n你好 "+username
+        else:
+            return "登录失败"
+
+    #这里缺一个注册功能
 
 
 
---
+++ init_users.py
@@ -0,0 +1,75 @@
+from flask import Flask
+# 导入配置好的相关数据库信息
+from flask_sqlalchemy import SQLAlchemy
+
+# 启动
+app = Flask(__name__)
+
+
+# 配置数据库并定义数据库表模型类
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost:3306/courses'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
+db = SQLAlchemy(app)
+# 创建用户类 存储用户信息 用于登录
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(80),unique=False)
+    password = db.Column(db.String(120),unique=False)
+    radio = db.Column(db.String(20),unique=False)
+
+    def __init__(self,username,password,radio):
+        self.username = username
+        self.password = password
+        self.radio  = radio
+
+# db.drop_all()
+# db.create_all()
+# admin0 = User('陆可','0001','教师')
+# admin1 = User('常雨','169094320','学生')
+# admin2 = User('代章福','169094321','学生')
+# admin3 = User('高忠','169094322','学生')
+# admin4 = User('李陈斌','169094325','学生')
+# admin5 = User('李雪','169094328','学生')
+# admin6 = User('刘兆柱','169094329','学生')
+# admin7 = User('柳亚雄','169144114','学生')
+# admin8 = User('牛良伟','169094330','学生')
+# admin9 = User('潘剑兵','169094331','学生')
+# admin10 = User('钱丹丹','169094333','学生')
+# admin11 = User('沈福莉','169094334','学生')
+# admin12 = User('时微尘','169094336','学生')
+# admin13 = User('谈成龙','169094337','学生')
+# admin14 = User('汪鹏','169094338','学生')
+# admin15 = User('汪艳','169094339','学生')
+# admin16 = User('王宇','169094340','学生')
+# admin17 = User('吴甜甜','169094341','学生')
+# admin18 = User('许淼','169094343','学生')
+# admin19 = User('许玉柱','169094344','学生')
+# admin20 = User('薛家升','169094345','学生')
+# admin21 = User('杨洁','169094346','学生')
+# admin22= User('叶甜甜','169094347','学生')
+# admin23 = User('张傲','169094348','学生')
+# admin24 = User('张程','169094349','学生')
+# admin25 = User('张建','169094350','学生')
+# admin26 = User('张结宝','169104612','学生')
+# admin27 = User('张伟','169094351','学生')
+# admin28 = User('周力','169094352','学生')
+# admin29 = User('朱海发','169094353','学生')
+# admin30 = User('朱泽明','169094354','学生')
+
+# db.session.add_all([admin0,admin1,admin2,admin3,admin4,admin5,admin6,admin7,admin8,admin9,admin10,admin11,admin12,admin13,admin14,admin15,admin16,admin17,admin18,admin19,admin20,admin21,admin22,admin23,admin24,admin25,admin26,admin27,admin28,admin29,admin30])
+# db.session.commit()
+print("11111111111")
+print(User.query.all())
+print(User.query.filter_by(username="李陈斌").first())
+print(User.query.filter_by(username="李陈").first())
+
+# if User.query.filter_by(username="李陈斌"):
+#     print("没有李陈斌")
+# else:
+#     print("有李陈斌")
+# if User.query.filter_by(username="李陈"):
+#     print("没有李陈")
+# else:
+#     print("有李陈")
+
